5f456d89bf16664ae18018a1b91decb37fb765fe
hcheikhaoui <hicham.cheikhaoui@docaposte.fr>
2021-05-15 16:48:48 +0100

    init commit , init project

---
+++ src/main/java/com/rest/api/demo/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.rest.api.demo.repository;
+
+import com.rest.api.demo.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+}
